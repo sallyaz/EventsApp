e1f229de60e2c10d34405e1e0942be155db416ca
Sally azulay <sally.azulay@xtrade.com>
2025-05-07 23:12:23 +0300

    refacor component to match assingment requests

--- backend/src/controllers/event.controller.ts
+++ backend/src/controllers/event.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { mockEvents } from '../mocks/events.mock';
-import { RSVP } from '../types/event.types.js';
+import { mockUsers } from '../mocks/users.mock';
 
 // GET /events
 export const getAllEvents = (_: Request, res: Response) => {
@@ -18,30 +18,65 @@ export const getEventById = (req: Request, res: Response) => {
 
 // POST /events/:id/rsvp
 // This endpoint allows a user to RSVP to an event
-// It expects a userId and the number of guests in the request body
 export const registerRSVP = (req: Request, res: Response) => {
   const { id } = req.params;
-  const { userId, guests } = req.body as RSVP;
-  if (!userId || typeof guests !== 'number') {
-    return res.status(400).json({ error: 'Invalid RSVP data' });
-  }
+  const { userName } = req.body ;
   const event = mockEvents.find(e => e.id === id);
+  const eventIndex = mockEvents.findIndex(e => e.id === id);
   if (!event) return res.status(404).json({ error: 'Event not found' });
 
-  res.status(201).json({ message: 'RSVP created', id, userId, guests });
-}
+  if(event.rsvpCount.participants.includes(userName)) {
+    return res.status(400).json({ error: 'User already RSVP' });
+  }
+  event.rsvpCount.participants.push(userName);
+  event.rsvpCount.guests += 1;
+  mockEvents[eventIndex] = event;
 
+  res.status(201).json({ message: 'RSVP created', mockEvents });
+}
+// PATCH /events/:id/rsvp
+// This endpoint allows a user to update their RSVP to an event
 export const updateRSVP = (req: Request, res: Response) => {
   const { id } = req.params;
-  const { guests } = req.body;
-  if (typeof guests !== 'number') {
-    return res.status(400).json({ error: 'Invalid guests count' });
-  }
+  const { title, location, date } = req.body;
+  const event = mockEvents.find(e => e.id === id);
+  const eventIndex = mockEvents.findIndex(e => e.id === id);
+  if (!event) return res.status(404).json({ error: 'Event not found' });
+  
+  if (title) event.title = title;
+  if (location) event.location = location;
+  if (date) event.date = date;
 
-  res.status(200).json({ message: 'RSVP updated', id, guests });
+  mockEvents[eventIndex] = event;
+
+  res.status(200).json({ message: 'RSVP updated' ,event: mockEvents[eventIndex] });
 };
-
+// DELETE /events/:id/rsvp
+// This endpoint allows a user to cancel their RSVP to an event
 export const cancelRSVP = (req: Request, res: Response) => {
   const { id } = req.params;
-  res.status(200).json({ message: `RSVP for event ${id} cancelled` });
+
+  const updateMockEvent = mockEvents.filter(e => e.id != id);
+  mockEvents.length = 0;
+  mockEvents.push(...updateMockEvent);
+
+  res.status(200).json({ message: `RSVP for event ${id} cancelled`, mockEvents });
+};
+
+export const userAuth = (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Missing credentials' });
+  }
+  const user = mockUsers.find(e => e.email === email );
+  if (user) {
+    const isPasswordCorrect = user.password === password;
+    if (isPasswordCorrect) {
+      const { password, ...userWithoutPassword } = user;
+      return res.status(200).json(userWithoutPassword);
+    }else {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+  }
+  return res.status(401).json({ error: 'User Not Found' }); 
 };
