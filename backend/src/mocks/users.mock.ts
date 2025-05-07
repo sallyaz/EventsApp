740aadf12de055f030e1c5bb0d08bcdf5ac0255f
Sally azulay <sally.azulay@xtrade.com>
2025-05-07 23:11:10 +0300

    added new users mock data

---
+++ backend/src/mocks/users.mock.ts
@@ -0,0 +1,16 @@
+import { User } from '../types/event.types.js';
+
+export const mockUsers: User[] = [
+  {
+    "id": 1,
+    "email": "sally@test.com",
+    "userName": "sally",
+    "password": "123456"
+  },
+  {
+    "id": 2,
+    "email": "test@test.com",
+    "userName": "Shai", 
+    "password": "12345"
+  },
+];
