747fa5ed8486d87425bde1d0e779cabd4a4bd114
Sally azulay <sally.azulay@xtrade.com>
2025-05-09 14:57:51 +0300

    added new global colors scheme

---
+++ frontend/src/constants/colors.ts
@@ -0,0 +1,9 @@
+export default {
+    blue: '#3498db',
+    lightGray: '#bdc3c7',
+    nearlyWhite: '#f4f8f7',
+    grey: '#7f8c8d',
+    textColor: '#1c1e21',
+    primary: '#3498db',
+    error: '#C53F3F',
+  };
