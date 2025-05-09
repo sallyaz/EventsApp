c1a729b93c31b05694e7aa09c326b3bf40257e38
Sally azulay <sally.azulay@xtrade.com>
2025-05-09 16:20:19 +0300

    changed the navigation to support real status from redux

--- frontend/src/navigation/AppNavigator.tsx
+++ frontend/src/navigation/AppNavigator.tsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
+import React, {use, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 // Navigation
 import MainNavigation from './MainNavigator';
 import AuthNavigation from './AuthNavigator';
+
+//Screens
 import SplashScreen from '../screens/SplashScreen/SplashScreen';
+
+// Redux
 import { navigationRef } from '../utils/navigationRef';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../app/store';
+import { checkAuthStatus } from '../services/auth/authThunk';
 
 
 const AppNavigation: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isAuth, setIsAuth] = useState(false); // Replace with real auth logic
+  const dispatch = useDispatch();
+
+  const isAuth = useSelector((state: RootState) => state.auth.isAuthenticated);
+  const isLoading = useSelector((state: RootState) => state.auth.isLoading);
+
+  useEffect(() => {
+      dispatch(checkAuthStatus());
+  }, [dispatch]);
 
   const renderNavigation = () => {
     if (isLoading) return <SplashScreen />;
