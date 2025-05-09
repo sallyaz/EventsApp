e673fe2527943d88994ef1cc920a7a1f70244ccf
Sally azulay <sally.azulay@xtrade.com>
2025-05-09 15:23:00 +0300

    configures store to support rtk query api calls

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../features/auth/authSlice';
+import authReducer from '../services/auth/authSlice';
+import { authApi } from '../services/auth/authApi';
 
 export const store = configureStore({
   reducer: {
+    [authApi.reducerPath]: authApi.reducer,
     auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
+
 });
 
 export type RootState = ReturnType<typeof store.getState>;
