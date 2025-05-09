83750425775789747e56080328eff6332bdd9513
Sally azulay <sally.azulay@xtrade.com>
2025-05-09 18:39:12 +0300

    set state where theres no information to show

--- frontend/src/screens/EventsScreen/EventsScreen.tsx
+++ frontend/src/screens/EventsScreen/EventsScreen.tsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {useAppSelector} from '../../hooks/useAppDispatch';
+import TextElement from '../../components/reusable/TextElemnt';
 
 const EventsScreen = () => {
+  const eventsInfo = useAppSelector((state: any) => state.events?.eventsInfo);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>EventsScreen</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      {!eventsInfo ? (
+        <View>
+          <Image
+            source={require('../../../assets/Events/eventsIcon.png')}
+            style={{width: 80, height: 80, alignSelf: 'center'}}
+            resizeMode="contain"
+          />
+          <TextElement customStyle={{fontSize: 16, fontWeight: '500', marginTop: 10}}>
+            There`s No Active Events. Please Try Later!
+          </TextElement>
+        </View>
+      ) : (
+        <Text>{JSON.stringify(eventsInfo)}</Text>
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default EventsScreen
+export default EventsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- frontend/src/screens/NotificationsScreen/NotificationsScreen.tsx
+++ frontend/src/screens/NotificationsScreen/NotificationsScreen.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { useAppSelector } from '../../hooks/useAppDispatch';
+import TextElement from '../../components/reusable/TextElemnt';
 
 const NotificationsScreen = () => {
+   const eventsInfo = useAppSelector((state: any) => state.events?.eventsInfo);
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>NotificationsScreen</Text>
-    </View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    {!eventsInfo ? (
+      <View>
+        <Image
+          source={require('../../../assets/Notifications/notificationIcon.png')}
+          style={{width: 80, height: 80, alignSelf: 'center'}}
+          resizeMode="contain"
+        />
+        <TextElement customStyle={{fontSize: 16, fontWeight: '500', marginTop: 10}}>
+          There`s No Waiting Notifications. Please Try Later!
+        </TextElement>
+      </View>
+    ) : (
+      <Text>{JSON.stringify(eventsInfo)}</Text>
+    )}
+  </View>
   )
 }
 
