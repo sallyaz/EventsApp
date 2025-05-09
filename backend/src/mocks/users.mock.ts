afdd9d2ed6d940574e228aa26bafe04275c1008b
Sally azulay <sally.azulay@xtrade.com>
2025-05-09 15:05:26 +0300

    changed users mock passwords data

--- backend/src/mocks/users.mock.ts
+++ backend/src/mocks/users.mock.ts
@@ -5,12 +5,12 @@ export const mockUsers: User[] = [
     "id": 1,
     "email": "sally@test.com",
     "userName": "sally",
-    "password": "123456"
+    "password": "12345678"
   },
   {
     "id": 2,
     "email": "test@test.com",
     "userName": "Shai", 
-    "password": "12345"
+    "password": "12121212"
   },
 ];
