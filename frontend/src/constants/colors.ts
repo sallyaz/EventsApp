22540824043baefabc30434f4847c5f854726240
Sally azulay <sally.azulay@xtrade.com>
2025-05-10 15:51:06 +0300

    removed unneeded colors

--- frontend/src/constants/colors.ts
+++ frontend/src/constants/colors.ts
@@ -1,9 +1,8 @@
 export default {
-    blue: '#3498db',
     lightGray: '#bdc3c7',
     nearlyWhite: '#f4f8f7',
-    grey: '#7f8c8d',
-    textColor: '#1c1e21',
     primary: '#3498db',
     error: '#C53F3F',
+    bgColor: '#ffffff',
+
   };
