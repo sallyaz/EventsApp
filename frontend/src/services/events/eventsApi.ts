bc44edc2d95bf005cac2ce39a12f32a38bab1b24
Sally azulay <sally.azulay@xtrade.com>
2025-05-10 15:57:20 +0300

    changed the ip in order to support off home cennection

--- frontend/src/services/events/eventsApi.ts
+++ frontend/src/services/events/eventsApi.ts
@@ -1,14 +1,20 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import { Event } from '../events/eventsSlice';
 
+interface PaginatedEventsResponse {
+  data: any[]; // Replace with Event[] if you have a type
+  currentPage: number;
+  totalPages: number;
+  totalItems: number;
+}
 
 export const eventsApi = createApi({
   reducerPath: 'eventsApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://192.168.10.4:3001',
+    baseUrl: 'http://192.168.1.253:3001', // replace with your machine's IP
   }),
   endpoints: (build) => ({
-    events: build.query<Event[], void>({
+    events: build.query<any[], void>({
       query: () => ({
         url: '/events',
         method: 'GET',
