e07c1d4c9e84268a168cb63dff2a55e36f4cbf94
Sally azulay <sally.azulay@xtrade.com>
2025-05-11 19:59:12 +0300

    changed imports from lowercase to uppercase component name

--- frontend/src/components/EventDetailsHelper/EventDetailsHelper.ts
+++ frontend/src/components/EventDetailsHelper/EventDetailsHelper.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {Alert} from 'react-native';
-import {onCreateTriggerNotification, onDisplayNotification} from '../notificationsHandler/notificationsHandler';
+import {onCreateTriggerNotification, onDisplayNotification} from '../NotificationsHandler/NotificationsHandler';
 
 export const RegisterRSVPHandler =
   (
