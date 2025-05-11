95501c9c4dd39778fadb08e1153a2ad6f6601066
Sally azulay <sally.azulay@xtrade.com>
2025-05-11 17:25:26 +0300

    new event card info component

---
+++ frontend/src/components/eventCardInfo/eventCardInfo.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { View, Image, Platform, StyleSheet } from 'react-native';
+import TextElement from '../reusable/TextElement';
+import ButtonElement from '../reusable/ButtonElement';
+import { navigate } from '../../utils/navigationRef';
+
+export type EventCardProps = {
+  id: number;
+  title: string;
+  date: string;
+  location: string;
+  rsvpCount: { participants: number; guests: number };
+  description: string;
+  image: string;
+};
+
+const EventCardItem = ({
+  id,
+  title,
+  date,
+  location,
+  rsvpCount,
+  description,
+  image,
+}: EventCardProps) => {
+  return (
+    <View
+      style={{
+        ...styles.item,
+        borderWidth: Platform.OS === 'ios' ? 0.5 : 1,
+        padding: 20,
+      }}>
+      <View style={{ flexDirection: 'column', justifyContent: 'space-between' }}>
+        <Image
+          source={require('../../../assets/Events/eventsIcon.png')}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <View style={{ marginVertical: 10 }}>
+          <TextElement customStyle={styles.title}>{title}</TextElement>
+          <TextElement>🗓️ Where: {location}</TextElement>
+          <TextElement>📍 When: {date}</TextElement>
+          <TextElement>👥 RSVP Count: {rsvpCount.guests}</TextElement>
+        </View>
+      </View>
+
+      <ButtonElement
+        title={'RSVP'}
+        onPress={() => {
+          navigate('EventsDetailsScreen', {
+            id,
+            title,
+            date,
+            location,
+            rsvpCount,
+            description,
+            image,
+          });
+          console.log(`👥 RSVP for ${title}`);
+        }}
+      />
+    </View>
+  );
+};
+
+export default EventCardItem;
+
+const styles = StyleSheet.create({
+  item: {
+    borderRadius: 15,
+    marginVertical: 10,
+    marginHorizontal: 16,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+  },
+  title: {
+    fontSize: 32,
+    marginVertical: 8,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    alignSelf: 'center',
+  },
+});
