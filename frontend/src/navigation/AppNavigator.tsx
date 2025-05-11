4cb93df52a2f8c7d08cbf61dfde28a325d24e029
Sally azulay <sally.azulay@xtrade.com>
2025-05-11 13:46:22 +0300

    added deepLink capability

--- frontend/src/navigation/AppNavigator.tsx
+++ frontend/src/navigation/AppNavigator.tsx
@@ -1,42 +1,53 @@
-import React, { useEffect} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import React, { useEffect, useRef, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
-// Navigation
-import MainNavigation from './MainNavigator';
-import AuthNavigation from './AuthNavigator';
-
-//Screens
-import SplashScreen from '../screens/SplashScreen/SplashScreen';
-
-// Redux
-import { navigationRef } from '../utils/navigationRef';
+//Redux 
 import { useSelector } from 'react-redux';
 import { RootState } from '../app/store';
 
-// Redux Thunk
-import { checkAuthStatus } from '../services/auth/authThunk';
+//Navigation
+import MainNavigation from './MainNavigator';
+import AuthNavigation from './AuthNavigator';
+import { navigationRef } from '../utils/navigationRef';
 
-// Hooks
+import SplashScreen from '../screens/SplashScreen/SplashScreen';
+import { checkAuthStatus } from '../services/auth/authThunk';
 import { useAppDispatch } from '../hooks/useAppDispatch';
 
+import { AppState, AppStateStatus, Linking } from 'react-native';
+
+
+const linking = {
+  prefixes: ['rsvpevents://'],
+  config: {
+    screens: {
+      EventsDetailsScreen: {
+        screens: {
+          EventsDetailsScreen: 'event/:id',
+        },
+      },
+    },
+  },
+};
 
 const AppNavigation: React.FC = () => {
   const dispatch = useAppDispatch();
+
+  const appState = useRef(AppState.currentState);
 
   const isAuth = useSelector((state: RootState) => state.auth.isAuthenticated);
   const isLoading = useSelector((state: RootState) => state.auth.isLoading);
 
   useEffect(() => {
-      dispatch(checkAuthStatus());
+    dispatch(checkAuthStatus())
   }, [dispatch]);
 
   const renderNavigation = () => {
     if (isLoading) return <SplashScreen />;
     return isAuth ? <MainNavigation /> : <AuthNavigation />;
   };
-
   return (
-    <NavigationContainer ref={navigationRef}>
+    <NavigationContainer linking={linking} ref={navigationRef}>
     {renderNavigation()}
     </NavigationContainer>
   );
